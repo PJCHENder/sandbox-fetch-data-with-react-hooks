70fb6b57918dd54931f8c6a86914ef5830c77680
PJCHENder <pjchender@gmail.com>
2019-08-28 11:54:01 +0800

    Init the layout and stylesheets

--- src/index.js
+++ src/index.js
@@ -9,13 +9,22 @@ import "./styles.css";
 
 function App() {
   return (
-    <div className="App pure-form">
+    <div className="App ">
       <h1>Fetch data with React Hooks</h1>
-      <div className="pure-control-group">
-        <input type="text" className="pure-input-rounded" />
-        <button type="button" className="pure-button button-small">
-          Search
-        </button>
+      <div className="pure-form">
+        <div className="pure-control-group">
+          <input type="text" className="pure-input-rounded" />
+          <button type="button" className="pure-button button-small">
+            Search
+          </button>
+        </div>
+      </div>
+      <div className="pure-menu">
+        <ul className="pure-menu-list">
+          <li className="pure-menu-item">Hello React</li>
+          <li className="pure-menu-item">Hello React Hooks</li>
+          <li className="pure-menu-item">Hello Sandbox</li>
+        </ul>
       </div>
     </div>
   );
