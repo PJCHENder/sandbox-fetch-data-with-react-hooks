e3d9742ea8793a9410a56caefec70066484167b3
PJCHENder <pjchender@gmail.com>
2019-08-28 15:10:22 +0800

    encapsulate into a custom effect to fetchData

--- src/index.js
+++ src/index.js
@@ -2,41 +2,15 @@
  * https://www.robinwieruch.de/react-hooks-fetch-data
  */
 
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import axios from "axios";
 
 import "./styles.css";
+import useFetchData from "./useFetchData";
 
 function App() {
   const [query, setQuery] = useState("");
-  const [fetchUrl, setFetchUrl] = useState(
-    "https://hn.algolia.com/api/v1/search"
-  );
-  const [data, setData] = useState({
-    hits: []
-  });
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setIsLoading(true);
-      setIsError(false);
-      try {
-        const response = await axios.get(fetchUrl);
-        setData({
-          hits: response.data.hits
-        });
-      } catch (error) {
-        setIsError(true);
-      }
-
-      setIsLoading(false);
-    };
-
-    fetchData();
-  }, [fetchUrl]);
+  const [{ data, isLoading, isError }, setFetchUrl] = useFetchData();
 
   return (
     <div className="App ">
@@ -74,7 +48,7 @@ function App() {
                 .filter(hit => hit.title)
                 .map(hit => (
                   <li className="pure-menu-item" key={hit.objectID}>
-                    <a href={hit.url} class="pure-menu-link">
+                    <a href={hit.url} className="pure-menu-link">
                       {hit.title}
                     </a>
                   </li>
