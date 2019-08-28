a5cf360beb80a718b34447590ed2016d718c76fb
pjchender <pjchender@gmail.com>
2019-08-28 15:52:17 +0800

    Put logic together by useReducer

--- src/useFetchData.js
+++ src/useFetchData.js
@@ -1,26 +1,62 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useReducer } from "react";
 import axios from "axios";
 
+const fetchDataReducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_INIT":
+      return {
+        ...state,
+        isLoading: true,
+        isError: false
+      };
+    case "FETCH_SUCCESS":
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload
+      };
+    case "FETCH_FAILED":
+      return {
+        ...state,
+        isLoading: false,
+        isError: true
+      };
+    default:
+      return state;
+  }
+};
+
 const useFetchData = ({ initialData, initialFetchUrl }) => {
+  const [{ data, isLoading, isError }, dispatch] = useReducer(
+    fetchDataReducer,
+    {
+      data: initialData,
+      isLoading: false,
+      isError: false
+    }
+  );
+
   const [fetchUrl, setFetchUrl] = useState(initialFetchUrl);
-  const [data, setData] = useState(initialData);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
-      setIsLoading(true);
-      setIsError(false);
+      dispatch({
+        type: "FETCH_INIT"
+      });
       try {
         const response = await axios.get(fetchUrl);
-        setData({
-          hits: response.data.hits
+        dispatch({
+          type: "FETCH_SUCCESS",
+          payload: {
+            hits: response.data.hits
+          }
         });
       } catch (error) {
-        setIsError(true);
+        dispatch({
+          type: "FETCH_FAILED"
+        });
       }
-
-      setIsLoading(false);
     };
 
     fetchData();
